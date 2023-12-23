f642ad0eb685c515b968f3c8597070475ccb2faa
Bilal Ali <bilalal495@outlook.com>
2023-12-22 23:13:52 -0500

    Update page styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,44 +1,45 @@
 import * as React from "react";
-import { FaGithub, FaLinkedinIn, FaEnvelope } from "react-icons/fa";
+import { FiGithub, FiLinkedin, FiMail } from "react-icons/fi";
 
 const IndexPage = () => {
   const iconStyle = () => {
-    return "text-3xl px-4 py-1 text-white rounded-full hover:text-black hover:bg-white";
+    return "text-3xl mx-4 hover:text-yellow-500";
   };
 
   const navStyle = () => {
-    return "text-lg px-4 hover:text-sky-500 cursor-pointer";
+    return "text-lg px-4 hover:text-yellow-500 cursor-pointer";
   };
 
   return (
-    <div className="grid place-content-center min-w-screen min-h-screen bg-sky-50">
+    <div className="grid place-content-center min-w-screen min-h-screen bg-zinc-100">
       {/* <ScrollProgress /> */}
       <div className="mb-20">
         <div className="text-center">
-          <h1 className="text-5xl font-semibold tracking-wide">Bilal Ali</h1>
+          <h1 className="text-6xl font-medium tracking-wide text-sky-700">
+            Bilal Ali
+          </h1>
           <h2 className="text-xl mt-5 tracking-wide font-light">
             computer science @ upenn
           </h2>
         </div>
 
-        <div className="flex justify-center mt-6 mx-auto py-1 px-1.5 rounded-full max-w-fit bg-sky-500">
+        <div className="flex justify-center mt-6 mx-auto py-1 px-1.5">
           <a className={iconStyle()} href="https://github.com/bilala45">
-            <FaGithub />
+            <FiGithub />
           </a>
           <a
             className={iconStyle()}
             href="https://www.linkedin.com/in/bilal-ali1/"
           >
-            <FaLinkedinIn />
+            <FiLinkedin />
           </a>
           <a className={iconStyle()} href="mailto: bilalali@seas.upenn.edu">
-            <FaEnvelope />
+            <FiMail />
           </a>
         </div>
+        <hr className="mt-8 border-t-2"></hr>
 
-        <hr className="mt-10 border-t-2 font-bold"></hr>
-
-        <div className="flex justify-center mt-6 mx-auto max-w-fit">
+        <div className="flex justify-center mt-4 mx-auto max-w-fit text-sky-700">
           <div className={navStyle()}>home</div>
           <div className={navStyle()}>about</div>
           <div className={navStyle()}>projects</div>
