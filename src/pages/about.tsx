4e5f2818c50e4bbe17ad85a19a9c8a1b59a43d85
Bilal Ali <bilalal495@outlook.com>
2023-12-23 17:08:51 -0500

    Add transition effect to header

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,9 +1,49 @@
 import * as React from "react";
+import { Link } from "gatsby";
 
 const AboutPage = () => {
+  const [translate, setTranslate] = React.useState(false);
+
+  // Trigger header translation on page load
+  React.useEffect(() => {
+    setTranslate(true);
+  }, []);
+
+  const navStyle = () => {
+    return "mx-4 text-lg hover:text-yellow-500";
+  };
+
+  const Header = () => {
+    return (
+      <div
+        className={`transition-transform duration-1000 ease-in-out min-h-screen pt-10 ${
+          translate ? "translate-y-0" : "translate-y-1/2"
+        }`}
+      >
+        <h1 className="text-5xl text-center font-medium tracking-wide text-sky-700">
+          Bilal Ali
+        </h1>
+
+        <div className="flex justify-center mt-2 mx-auto text-sky-700">
+          <div className={navStyle()}>
+            <Link to="/">home</Link>
+          </div>
+          <div className={navStyle()}>
+            <Link to="/about">about</Link>
+          </div>
+          <div className={navStyle()}>
+            <Link to="/projects">projects</Link>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
   return (
-    <div className="grid place-content-center min-w-screen min-h-screen bg-zinc-100">
-      <div className="max-w-xl mx-6 text-md">
+    <div className="min-w-screen min-h-screen bg-zinc-100">
+      {Header()}
+
+      {/* <div className="max-w-xl mx-6 text-md">
         <div className="font-semibold text-lg">about</div>
         <div className="pt-3">
           Hi! I'm a Master's student studying computer science at Penn. I'm
@@ -23,7 +63,7 @@ const AboutPage = () => {
         <div className="pt-3">
           Graduated from McGill University (spring 21).
         </div>
-      </div>
+      </div> */}
     </div>
   );
 };
