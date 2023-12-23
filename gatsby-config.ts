10cb1dba0ab4201f30bf74472bd7d51207ace1ce
Bilal Ali <bilalal495@outlook.com>
2023-12-22 23:37:44 -0500

    Update config files with url

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -2,14 +2,14 @@ import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `Personal Website`,
-    siteUrl: `https://www.yourdomain.tld`
+    title: `Bilal Ali`,
+    siteUrl: `https://bilalali.dev`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-postcss"]
+  plugins: ["gatsby-plugin-postcss"],
 };
 
 export default config;
