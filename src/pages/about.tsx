5692d35c1720c9fa946d9aa2bbf06e6d47478d53
Bilal Ali <bilalal495@outlook.com>
2023-12-23 00:08:41 -0500

    Update description on about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -2,26 +2,27 @@ import * as React from "react";
 
 const AboutPage = () => {
   return (
-    <div className="grid place-content-center min-w-screen min-h-screen bg-zinc-100 text-md">
-      <div>
-        Hi! I'm a Master's student at the University of Pennsylvania studying
-        computer science. I'm interested in backend engineering and large scale
-        distributed systems.
-      </div>
+    <div className="grid place-content-center min-w-screen min-h-screen bg-zinc-100">
+      <div className="max-w-xl mx-6 text-md">
+        <div className="font-semibold text-lg">about</div>
+        <div className="pt-3">
+          Hi! I'm a Master's student studying computer science at Penn. I'm
+          interested in backend engineering and large scale distributed systems.
+        </div>
 
-      <div className="pt-5 font-semibold text-lg">previously</div>
-      <div className="pt-3">
-        Spent Summer 2023 writing backend code for cloud-based microservices at
-        Genesys.
-      </div>
-      <div className="pt-2">
-        Helped develop a web app for a small biotech startup called Normative.
-      </div>
-      <div className="pt-2">Graduated from McGill University in 2021.</div>
-
-      <div className="pt-5 font-semibold text-lg">interests</div>
-      <div className="pt-3">
-        backend engineering, software system design, distributed systems
+        <div className="pt-8 font-semibold text-lg">what im up to</div>
+        <div className="pt-3">??? (summer 24).</div>
+        <div className="pt-3">
+          Wrote backend code for cloud-based microservices at Genesys (summer
+          23).
+        </div>
+        <div className="pt-3">
+          Developed a web app for a small biotech startup called Normative
+          (spring 22).
+        </div>
+        <div className="pt-3">
+          Graduated from McGill University (spring 21).
+        </div>
       </div>
     </div>
   );
